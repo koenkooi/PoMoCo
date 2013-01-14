8c40989c0f0726497585b030acc99887c94868d3
Koen Kooi <koen@dominion.thruhere.net>
2013-01-14 11:55:31 +0100

    Move{Forward,Backward}: parametrize and speed up
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- Moves/MoveBackward.py
+++ Moves/MoveBackward.py
@@ -5,7 +5,8 @@ import time
 deg = 25
 midFloor = 30
 hipSwing = 25
-pause = 0.5
+pause = 0.1
+steptime = 0.05
 
 # tripod1 = RF,LM,RB
 # tripod2 = LF,RM,LB
@@ -13,25 +14,25 @@ pause = 0.5
 for timeStop in range(2):
     # replant tripod2 backwards while tripod1 moves forward
     #   relpant tripod 2 backwards
-    hexy.LF.replantFoot(deg+hipSwing,stepTime=0.5)
-    hexy.RM.replantFoot(-hipSwing,stepTime=0.5)
-    hexy.LB.replantFoot(-deg+hipSwing,stepTime=0.5)
+    hexy.LF.replantFoot(deg+hipSwing,stepTime=steptime)
+    hexy.RM.replantFoot(-hipSwing,stepTime=steptime)
+    hexy.LB.replantFoot(-deg+hipSwing,stepTime=steptime)
 
     #   tripod1 moves forward
-    hexy.RF.setHipDeg(-deg+hipSwing,stepTime=0.5)
-    hexy.LM.setHipDeg(-hipSwing,stepTime=0.5)
-    hexy.RB.setHipDeg(deg+hipSwing,stepTime=0.5)
-    time.sleep(0.6)
+    hexy.RF.setHipDeg(-deg+hipSwing,stepTime=steptime)
+    hexy.LM.setHipDeg(-hipSwing,stepTime=steptime)
+    hexy.RB.setHipDeg(deg+hipSwing,stepTime=steptime)
+    time.sleep(pause)
 
     # replant tripod1 backwards while tripod2 moves behind
     #   replant tripod1 backwards
-    hexy.RF.replantFoot(-deg-hipSwing,stepTime=0.5)
-    hexy.LM.replantFoot(hipSwing,stepTime=0.5)
-    hexy.RB.replantFoot(deg-hipSwing,stepTime=0.5)
+    hexy.RF.replantFoot(-deg-hipSwing,stepTime=steptime)
+    hexy.LM.replantFoot(hipSwing,stepTime=steptime)
+    hexy.RB.replantFoot(deg-hipSwing,stepTime=steptime)
 
     #   tripod2 moves behind
-    hexy.LF.setHipDeg(deg-hipSwing,stepTime=0.5)
-    hexy.RM.setHipDeg(hipSwing,stepTime=0.5)
-    hexy.LB.setHipDeg(-deg-hipSwing,stepTime=0.5)
-    time.sleep(0.6)
+    hexy.LF.setHipDeg(deg-hipSwing,stepTime=steptime)
+    hexy.RM.setHipDeg(hipSwing,stepTime=steptime)
+    hexy.LB.setHipDeg(-deg-hipSwing,stepTime=steptime)
+    time.sleep(pause)
 
--- Moves/MoveForward.py
+++ Moves/MoveForward.py
@@ -5,7 +5,8 @@ import time
 deg = 25
 midFloor = 30
 hipSwing = 25
-pause = 0.5
+pause = 0.1
+steptime = 0.05
 
 #tripod1 = RF,LM,RB
 #tripod2 = LF,RM,LB
@@ -14,24 +15,24 @@ for timeStop in range(2):
     #time.sleep(0.1)
     # replant tripod2 forward while tripod1 move behind
     #   relpant tripod 2 forward
-    hexy.LF.replantFoot(deg-hipSwing,stepTime=0.5)
-    hexy.RM.replantFoot(hipSwing,stepTime=0.5)
-    hexy.LB.replantFoot(-deg-hipSwing,stepTime=0.5)
+    hexy.LF.replantFoot(deg-hipSwing,stepTime=steptime)
+    hexy.RM.replantFoot(hipSwing,stepTime=steptime)
+    hexy.LB.replantFoot(-deg-hipSwing,stepTime=steptime)
 
     #   tripod1 moves behind
-    hexy.RF.setHipDeg(-deg-hipSwing,stepTime=0.5)
-    hexy.LM.setHipDeg(hipSwing,stepTime=0.5)
-    hexy.RB.setHipDeg(deg-hipSwing,stepTime=0.5)
-    time.sleep(0.6)
+    hexy.RF.setHipDeg(-deg-hipSwing,stepTime=steptime)
+    hexy.LM.setHipDeg(hipSwing,stepTime=steptime)
+    hexy.RB.setHipDeg(deg-hipSwing,stepTime=steptime)
+    time.sleep(pause)
 
     # replant tripod1 forward while tripod2 move behind
     #   replant tripod1 forward
-    hexy.RF.replantFoot(-deg+hipSwing,stepTime=0.5)
-    hexy.LM.replantFoot(-hipSwing,stepTime=0.5)
-    hexy.RB.replantFoot(deg+hipSwing,stepTime=0.5)
+    hexy.RF.replantFoot(-deg+hipSwing,stepTime=steptime)
+    hexy.LM.replantFoot(-hipSwing,stepTime=steptime)
+    hexy.RB.replantFoot(deg+hipSwing,stepTime=steptime)
 
     #   tripod2 moves behind
-    hexy.LF.setHipDeg(deg+hipSwing,stepTime=0.5)
-    hexy.RM.setHipDeg(-hipSwing,stepTime=0.5)
-    hexy.LB.setHipDeg(-deg+hipSwing,stepTime=0.5)
-    time.sleep(0.6)
+    hexy.LF.setHipDeg(deg+hipSwing,stepTime=steptime)
+    hexy.RM.setHipDeg(-hipSwing,stepTime=steptime)
+    hexy.LB.setHipDeg(-deg+hipSwing,stepTime=steptime)
+    time.sleep(pause)
